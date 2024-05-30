d3d7b58f7b1d7c4d4009d26d5da22487f2978d36
Sigerandi <erandisoublett319@aragon.unam.mx>
2024-05-29 19:09:44 -0600

    Se agrego comentarios

--- html/main.js
+++ html/main.js
@@ -1,11 +1,13 @@
+//creo una funcion que se ejecutara cuando hago click en el boton
 function muestra_oculta(id) {
-   
-    let div = document.getElementById(id);
-   
-    if (div.style.display == "none"){ 
-      div.style.display = "flex";
-    } 
-    else {
-      div.style.display = "none";
-    }
+  //declaro la funcion y paso por parametro el selector de referencia que quiero ocultar/mostrar
+  let div = document.getElementById(id);
+  //uso el condicional IF para saber si el DIV esta oculto (Display: none) o si esta visible 
+  if (div.style.display == "none"){ 
+    div.style.display = "flex";
+  } 
+  else {
+    div.style.display = "none";
+  }
 }
+
